901abf2b4cf2d09a22f17d3b53c26e7aad17892a
saibhuvana <131589403+saibhuvana@users.noreply.github.com>
2023-08-16 19:26:28 +0530

    Delete celsius.py

--- celsius.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-X=np.array([[68],[104],[140],[176],[212]])
-y=np.array([[20],[40],[60],[80],[100]])
-model=LinearRegression()
-model.fit(X,y)
